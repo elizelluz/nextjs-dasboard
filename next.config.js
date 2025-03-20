1182d458dfc1bc565668949878825c56d7c94749
elizabeth salcedo <esalcedo@todotek.net>
2025-03-19 19:18:25 -0500

    Update route.ts

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Aquí van tus opciones de configuración, por ejemplo:
-  reactStrictMode: true,
-  swcMinify: true,
-  // Agrega cualquier otra opción que necesites
-};
-
-module.exports = nextConfig;
